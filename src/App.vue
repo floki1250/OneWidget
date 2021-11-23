97102325dde2c486bdf0ba64de405b821ea0f0d1
Adem <37814393+floki1250@users.noreply.github.com>
2021-11-23 17:32:13 +0100

    First COMMIT

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="outer-circle">
+    <div v-for="i in 5" :key="i">
+      <div>
+        <q-avatar size="60px" font-size="20px" color="teal" text-color="white"
+          >A</q-avatar
+        >
+      </div>
+    </div>
+    <div id="inner-circle">
+      <img
+        src="../src/assets/logo.png"
+        alt=""
+        srcset=""
+        width="100"
+        height="100"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
